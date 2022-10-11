462a5e22b0493450f7605763b7d09c0f1c36f842
CarollCoelho <92138657+CarollCoelho@users.noreply.github.com>
2022-10-11 03:08:35 -0300

    Add files via upload

--- TCC_pet_e_gato - oficial/_layout-carol/_bd/create_petgato.sql
+++ TCC_pet_e_gato - oficial/_layout-carol/_bd/create_petgato.sql
@@ -13,7 +13,8 @@ CREATE TABLE cliente (
     nome_pet varchar(30) NOT NULL,
     nasc_pet varchar(10) NOT NULL,
 
-    primary key(cpf))
+    primary key(cpf)
+);
 
 
 CREATE TABLE administrador ( 
@@ -31,29 +32,16 @@ CREATE TABLE funcionario (
     email varchar(30) NOT NULL, 
     senha varchar (20) NOT NULL,
     data_nasc varchar(10) NOT NULL,  
-    primary key(matricula))
+    primary key(matricula)
+);
+
+CREATE TABLE servicos ( 
+   	servico varchar (30) NOT NULL,
+    descricao varchar(200) NOT NULL, 
+    preco varchar(30) NOT NULL, 
+    primary key(servico)
+ 
+);
 
 
-<html>
-    <head>
-        <title>Cadastro de usuários</title>
-        <meta charset="UTF-8">
 
-    <link rel="stylesheet" href="bootstrap.min.css">
-    </head>
-    <body>
-        <h1>Cadastre-se:</h1>
-        <form action="usuarios_cadastrados.php" method="POST"> 
-            <input type="hidden" name="operacao" value="inserir">
-            <p>Nome: <input type="text" name="nome"></p>
-            <p>E-mail: <input type="text" name="email"></p>
-            <p>Data de Nascimento: <input type="date" name="data_nasc"></p>
-            <p>Digite seu CPF:</p> <input type="text" name="cpf" \
-			    pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" \
-			    title="Digite um CPF no formato: xxx.xxx.xxx-xx">
-            <p>Endereço: <input type="text" name="endereco"></p>
-            
-            <p><input type="submit" value="Enviar"></p> 
-        </form>
-    </body>    
-</html>
