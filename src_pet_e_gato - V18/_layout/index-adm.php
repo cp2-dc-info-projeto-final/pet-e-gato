8171a5fa35bca6f07baa86f016d2d341073b7702
biaeiras <95935490+biaeiras@users.noreply.github.com>
2022-12-17 19:59:15 -0300

    Add files via upload

---
+++ src_pet_e_gato - V18/_layout/index-adm.php
@@ -0,0 +1,109 @@
+<?php include "autentica-adm.php" ?>
+
+
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <title>Dashboard | Pet&Gatô</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="_img/logo_petgato.png" type="image/x-icon" />
+    <link rel="stylesheet" href="_css/main.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+      integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </head>
+  <body class="fadeIn">
+
+
+    <div id="header">
+        <div class="container">
+          <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
+              
+              <a class="navbar-brand" href="#"></a>
+              <a href="index-adm.php" ><img src="_img/logo_petgato.png" class="img-center" width="25%"/></a>
+              
+              <button 
+                class="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#navbarNav" 
+                aria-controls="navbarNav" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index-adm.php"> Início</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link" href="servicos-adm.php">Serviços</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link" href="clientes.php">Clientes</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link" href="funcionario.php">Funcionários</a>
+                  </li>
+
+                  <ul class="navbar-nav nav-item mr-auto">
+                          <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Cadastrar</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="cadastro_servicos.php">Serviços</a>
+                              <a class="dropdown-item" href="cadastro_funcionario.php">Funcionários</a>
+                              <a class="dropdown-item" href="cadastro_adm.php">Administrador</a>
+                            </div>
+                          </li>
+                  </ul>
+                 
+
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contato</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Sobre</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link" href="logout-adm.php">Sair</a>
+                  </li>
+ 
+                   </ul>
+              </div>
+          </nav>
+        </div>
+    </div>
+
+<div id="slider">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 align-self-center">
+        <h1>O PetShop mais delicioso para o seu pet!</h1>
+        <h4>Contrate já um de nossos serviços.</h4>
+        <a href="#" class="btn btn-primary button">Contato</a>
+        <a href="#" class="btn btn-outline-primary button">Serviços</a>
+      </div>
+      <div class="col-md-6 align-self-center text-center">
+        <img src="_img/pet_img.svg" class="img-fluid" alt="Um gatinho e um cachorrinho.">
+      </div>
+    </div>
+  </div>
+
+
+</div>    
+    
+    <script src="js/bootstrap.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
